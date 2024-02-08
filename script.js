217fbb2a03f29697a4378471f210ed297d67166a
asimovc <142914286+asimovc@users.noreply.github.com>
2024-02-08 11:00:44 -0400

    Add hover event to grid

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const grid = document.querySelector('.container');
 for (let i = 0; i < 16; i++) {
 	for (let j = 0; j < 16; j++) {
 		const sqr = document.createElement('div');
+		sqr.classList.add("square");
 		grid.appendChild(sqr);
 	}
 }
+
+const hover = document.querySelector('.container');
+
+hover.addEventListener("mouseover", (event) => {
+	event.target.style.background = "red";
+});
