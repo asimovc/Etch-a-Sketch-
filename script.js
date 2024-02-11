ccd5a56c8445664a1fa3ee9a7d3deb52760e2aee
asimovc <142914286+asimovc@users.noreply.github.com>
2024-02-11 15:10:08 -0400

    Add reset grid and button event handler

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const grid = document.querySelector('.container');
+const btn = document.querySelector('#btn');
+const hover = document.querySelector('.container');
+let size = makeGrid(16);
 
-for (let i = 0; i < 16; i++) {
-	for (let j = 0; j < 16; j++) {
-		const sqr = document.createElement('div');
-		sqr.classList.add("square");
-		grid.appendChild(sqr);
+function makeGrid(size){
+	for (let i = 0; i < size; i++) {
+		for (let j = 0; j < size; j++) {
+			const sqr = document.createElement('div');
+			sqr.classList.add("square");
+			grid.appendChild(sqr);
+		}
 	}
 }
 
-const hover = document.querySelector('.container');
+function newGrid(size){
+	grid.innerHTML='';
+
+	const squareSize = document.querySelector(':root');
+
+	squareSize.style.setProperty('--size', `${size}`);
+
+	makeGrid(size);
+}
 
 hover.addEventListener("mouseover", (event) => {
 	event.target.style.background = "red";
 });
+
+btn.addEventListener('click', (event) => {
+	size = prompt('Give the size of the grid');
+
+	newGrid(size);
+})
